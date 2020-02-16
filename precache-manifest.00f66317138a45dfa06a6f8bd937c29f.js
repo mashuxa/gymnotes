f7591bf0b1d3e9ba3e81c71405aeb66e99afcb96
Your Name <you@example.com>
2020-02-16 22:44:41 +0300

    Updates

---
+++ precache-manifest.00f66317138a45dfa06a6f8bd937c29f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7adca765bc9c405f64ea2e926020db29",
+    "url": "/gymnotes/index.html"
+  },
+  {
+    "revision": "b6c2f26380197618dafe",
+    "url": "/gymnotes/static/css/main.1ffbef73.chunk.css"
+  },
+  {
+    "revision": "24e3972a6bf74bde9756",
+    "url": "/gymnotes/static/js/2.c9ffe093.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/gymnotes/static/js/2.c9ffe093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c2f26380197618dafe",
+    "url": "/gymnotes/static/js/main.1602b52e.chunk.js"
+  },
+  {
+    "revision": "616199243c909dca47b5",
+    "url": "/gymnotes/static/js/runtime-main.b55333e4.js"
+  },
+  {
+    "revision": "6e4f7313763a948fa88cee3fea4856fb",
+    "url": "/gymnotes/static/media/exercise.6e4f7313.svg"
+  },
+  {
+    "revision": "eaa7aac42ffc1cfe152ad41211f9c98b",
+    "url": "/gymnotes/static/media/history.eaa7aac4.svg"
+  },
+  {
+    "revision": "4014f93e2eaca9b7670196027e396120",
+    "url": "/gymnotes/static/media/letter.4014f93e.svg"
+  },
+  {
+    "revision": "7dd3a09dbe70e1c96d0fa469fa280559",
+    "url": "/gymnotes/static/media/list.7dd3a09d.svg"
+  },
+  {
+    "revision": "dff3a5a5ba48956742dffa528d3422d9",
+    "url": "/gymnotes/static/media/menu.dff3a5a5.svg"
+  }
+]);
